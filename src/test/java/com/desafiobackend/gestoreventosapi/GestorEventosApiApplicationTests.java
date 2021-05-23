45db0304a65d816d8cdfbc1457b962319ba3de6d
jorge <jorge.fernando.brito@gmail.com>
2021-05-23 12:00:02 -0300

    estruturação inicial do projeto

---
+++ src/test/java/com/desafiobackend/gestoreventosapi/GestorEventosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiobackend.gestoreventosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorEventosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
